08ee3caa9b9bc60c9f070b5966a9e6047bd0cce9
Ramadhan306 <ramadhanabdallahsaid@gmail.com>
2025-07-28 01:33:33 +0300

    refactor: Convert portfolio styling to TailwindCSS

---
+++ portfolio-project/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Looks for all .html and .js files in the root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
