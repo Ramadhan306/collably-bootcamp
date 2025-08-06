9e8081699753a8e5400e975de7a1a515597666d6
Ramadhan306 <ramadhanabdallahsaid@gmail.com>
2025-08-06 03:15:38 +0300

    Add script.js

---
+++ portfolio-project/script.js
@@ -0,0 +1,26 @@
+// Hello from script.js!
+console.log("Hello from script.js!");
+
+// Declare variables using const
+const name = "Ramadhani";
+const age = 25;
+const isStudent = true;
+
+// Log each variable to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Work with data using template literals
+let info = `My name is ${name} and I am ${age} years old.`;
+console.log("Info:", info);
+
+// Perform calculations
+const favNum1 = 7;
+const favNum2 = 3;
+
+const sum = favNum1 + favNum2;
+console.log("Sum:", sum);
+
+const product = favNum1 * favNum2;
+console.log("Product:", product);
